ea289c3fef3194ae03f196dbd78d05c4523b3f91
Pratham Jha <prathamjha5683@gmail.com>
2021-08-01 14:17:44 +0530

    Assignment completed SASS

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@ module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
     return res.render('home', {
-        title: "Home"
+        title: "Home Page"
     });
 }
 
--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-
+const scssmiddleware=require('node-sass-middleware');
 //used for creating session cookie
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local');
-
 const MongoStore=require('connect-mongo');
 
+app.use(scssmiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+}));
 app.use(express.urlencoded());
 
 app.use(cookieParser());
